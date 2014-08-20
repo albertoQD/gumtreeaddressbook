c8107098927d804f0fa79247e971eb0d09136af3
Alberto Quintero Delgado <ajquinterod@gmail.com>
2014-08-20 23:03:24 +0200

    Initial commit - basic structure

---
+++ GumtreeAddressBook.java
@@ -0,0 +1,30 @@
+package com.aqd.gumtreeaddressbook;
+
+import java.util.TreeSet;
+
+/**
+ *
+ * @author Alberto Quintero Delgado
+ */
+public class GumtreeAddressBook {
+
+    private class Person {
+        
+    }
+    
+    public void readFile() {
+        
+    }
+    
+    final static String FILE_ = "/home/ajqd/people.txt";
+    /* BST - O (log(N)) for insertion and lookup */
+    private TreeSet<Person> people_;
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
