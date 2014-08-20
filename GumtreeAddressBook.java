ff4911c4b93b64d020a6e3cc0167b1158d46d9c4
Alberto Quintero Delgado <ajquinterod@gmail.com>
2014-08-20 23:18:42 +0200

    Adding some parts of the reading

--- GumtreeAddressBook.java
+++ GumtreeAddressBook.java
@@ -1,5 +1,8 @@
 package com.aqd.gumtreeaddressbook;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Scanner;
 import java.util.TreeSet;
 
 /**
@@ -10,7 +13,7 @@ public class GumtreeAddressBook {
 
     private class Person {
         private final String  name_;
-        private final boolean sex_;
+        private final boolean sex_; /* 1- Male, 0 - Female */
         
         Person(String name, boolean sex) {
             name_ = name;
@@ -22,8 +25,29 @@ public class GumtreeAddressBook {
         
     }
     
-    public void readFile() {
+    public void readFile() throws IOException {
+        Scanner fileScanner = new Scanner(Paths.get(FILE_));
+        people_ = new TreeSet();
         
+        while(fileScanner.hasNextLine()) {
+            /* While reading the parts, if one of them is missing just 
+             * ignore this person and move forward */
+            Scanner lineScanner = new Scanner(fileScanner.nextLine());
+            lineScanner.useDelimiter(",");
+            
+            /* Checking for the person's name */
+            if (!lineScanner.hasNext()) continue;
+            String personName = lineScanner.next().trim();
+            
+            /* Checking for the person's sex */
+            if (!lineScanner.hasNext()) continue;
+            boolean personSex = (lineScanner.next()
+                                            .trim()
+                                            .equalsIgnoreCase("male"));
+            
+            // Add it to the TreeSet
+            
+        }
     }
     
     final static String FILE_ = "/home/ajqd/people.txt";
@@ -32,9 +56,11 @@ public class GumtreeAddressBook {
     
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        GumtreeAddressBook app = new GumtreeAddressBook();
+        app.readFile();
     }
     
 }
